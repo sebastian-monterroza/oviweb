edace8db4b38ab62285ee36fa38473777d4efe0a
sebastian-monterroza <137902095+sebastian-monterroza@users.noreply.github.com>
2025-03-31 19:05:57 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById("menu").classList.add("active");
+    document.getElementById("overlay").classList.add("active");
+}
+
+function closeMenu() {
+    document.getElementById("menu").classList.remove("active");
+    document.getElementById("overlay").classList.remove("active");
+}
